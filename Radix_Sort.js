659619c23b04534aa83a6b78bb0e9ba5027b86dd
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-14 12:49:33 -0500

    Radix Sort: Explanation

---
+++ Radix_Sort.js
@@ -0,0 +1,11 @@
+// RADIX SORT
+// Radix Sort is a special sorting algorithm that works on lists of numbers
+// It never makes comparisons between elements
+// It exploits the fact that information about the size of a number is encoded in the number of digits
+// More digits means a bigger number
+
+// The amount of buckets depends on the base of the numbers.
+// Base 10 has 10 buckets, base 7 has 7 buckets.
+
+// You split up the numbers based on how many digits the largest number has
+// For example: 4579 is the largest number is an array, then it will take 4 iterations to sort the entire array.

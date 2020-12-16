4dca29d0b2bd1a2f20497210b239aed6efee9e50
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-16 17:03:12 -0500

    DDL: Set UP

---
+++ Doubly_Linked_list.js
@@ -0,0 +1,25 @@
+// Node
+// - val
+// - next
+// - prev
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+// Doubly Linked List
+// - head
+// - tail
+// - length
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}

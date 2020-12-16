15f31ad293ef7c16817650c489e41b86733c07e6
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-15 21:21:48 -0500

    Singly Linked List: Shift

--- Singly_Linked_List.js
+++ Singly_Linked_List.js
@@ -55,6 +55,20 @@ class SinglyLinkedList {
 
     return current;
   }
+
+  shift() {
+    if (!this.head) return undefined;
+
+    var currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+
+    if (this.length === 0) {
+      this.tail = null;
+    }
+
+    return currentHead;
+  }
 }
 
 // var first = new Node("Hello");

a08413b145b76851d13bd97f06cf98fb4b523edd
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-17 13:13:18 -0500

    S&Q: Stacks as an Array

--- Stacks_Queues.js
+++ Stacks_Queues.js
@@ -5,3 +5,17 @@
 // Manage Function Incovation
 // Undo/Redo
 // Routing (the History Object on a browser)
+
+//var stack = [];
+// This is from the end
+stack.push(55); // Push adds a value to the end of a stack/array
+stack.pop(); // Pop removes tha last item
+
+// This is from the beginning
+stack.unshift(50); // Adds to the beginning
+stack.unshift(25); // Adds to the begginning
+stack.shift(); // Removes first item in stack
+
+// Effeciency
+// Push and Pop are more effecient since you don't have to
+// reindex the array. That is if we are using arrays.

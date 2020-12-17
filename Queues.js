9877d3acb7b7ea60a7a89647cb43c9e7de43768a
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-17 14:49:36 -0500

    Queue: Array Queue

--- Queues.js
+++ Queues.js
@@ -8,4 +8,12 @@
 // Shopping Line
 
 // Queue using an array
+// When using an array as a Queue
+// You can use unshift() combined with pop()
+// or
+// push() combined with shift()
+
 var q = [];
+
+// Linked List based Queue
+// Performance is better than an array

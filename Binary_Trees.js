1517200338a2e55c3dc9df887c4be64fa2dbb124
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-18 14:37:03 -0500

    BST: Use Cases

--- Binary_Trees.js
+++ Binary_Trees.js
@@ -1,7 +1,16 @@
-// Binary Trees
+// Binary Search Trees
 // Root - Top most node
 // Child - A node connected directly under to another node
 // Parent - A node connected one level above another node
 // Siblings - A group of nodes with the same parent node
 // Leaf - A node with no children
 // Edge - The connection between one node and another
+
+// Use Cases for Trees
+// HTML DOM (Document Object Model)
+// Network Routing: i.e. Shortest Path
+// Abstract Syntax Tree
+// Artificial Intelligence:
+//   Tic Tac Toe
+//   Decision tree
+// Folder Structure on Computer

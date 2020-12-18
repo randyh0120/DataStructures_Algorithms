4fa81842d40cf633f2df1db39e2b99fc648e71b8
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-18 15:03:37 -0500

    Trees: BST

--- Binary_Trees.js
+++ Binary_Trees.js
@@ -1,4 +1,4 @@
-// Binary Search Trees
+//// Trees
 // Root - Top most node
 // Child - A node connected directly under to another node
 // Parent - A node connected one level above another node
@@ -6,7 +6,7 @@
 // Leaf - A node with no children
 // Edge - The connection between one node and another
 
-// Use Cases for Trees
+//// Use Cases for Trees
 // HTML DOM (Document Object Model)
 // Network Routing: i.e. Shortest Path
 // Abstract Syntax Tree
@@ -14,3 +14,15 @@
 //   Tic Tac Toe
 //   Decision tree
 // Folder Structure on Computer
+
+//// Types of Trees
+
+// BST:
+// Can only have 2 Children per Node
+// BST is a great way to store data that can compared
+// Ex. Left is always less than the right.
+//              10
+//             /  \
+//            8    12
+//           / \   / \
+//          6   9 11  15

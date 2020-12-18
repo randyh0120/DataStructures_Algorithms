c546c347a003f9938897ac3f3f1e28f8c3228e53
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-18 14:15:15 -0500

    BT: Definitions

---
+++ Binary_Trees.js
@@ -0,0 +1,6 @@
+// Binary Trees
+// Top most node is called the Root
+// Child - A node connected directly under to another node
+// Parent - A node connected one level above another node
+// Siblings - A group of nodes with the same parent node
+// Leaf - A node with no children

b563a169b80e6badd6647b57a599cc59a26469c0
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-17 20:10:37 -0500

    Queue: Complete

--- Queues.js
+++ Queues.js
@@ -1,7 +1,7 @@
 // Queue
 // FiFo
 
-// You add to the begginng and remove from the end.
+// You add to the end and remove from the beginning.
 
 // Ex.
 // Printers
@@ -12,8 +12,52 @@
 // You can use unshift() combined with pop()
 // or
 // push() combined with shift()
-
 var q = [];
 
 // Linked List based Queue
 // Performance is better than an array
+// You add to the end and remove from the beginning.
+class Queue {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList_Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  enQueue(val) {
+    var newNode = new Queue(val);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    return ++this.length;
+  }
+
+  deQueue(val) {
+    if (!this.first) return null;
+
+    var temp = this.first;
+
+    if (this.length === 1) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.temp.next = null;
+    this.length--;
+
+    return temp.val;
+  }
+}

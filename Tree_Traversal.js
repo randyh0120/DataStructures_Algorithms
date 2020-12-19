243cce416439dcf8c43cc207f6b8dbd73027c17c
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-19 16:16:52 -0500

    TT: BFS & DFS Definitions

---
+++ Tree_Traversal.js
@@ -0,0 +1,14 @@
+// Traversals
+
+// BFS (Breath First Search)
+// Left to Right
+
+// DFS (Depth First Search)
+// Top to Bottom
+// 3 Main Orders
+// 1. In Order
+//      Starting from the bottom to the top like Normal
+// 2. Pre-Order
+//      Top to Bottom like Normal
+// 3. Post-Order
+//      Bottom to Top in Levels or Depth

5869982df4cffea31279c26822dba35e277b316e
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-20 14:18:44 -0500

    Priority Queue: Use Cases

---
+++ Priority_Queue.js
@@ -0,0 +1,7 @@
+// Priority Queue
+
+// Use Cases
+// Anytime you have to keep retrieve items based on priority.
+// Hospital System. i.e critical health level 1 2 3
+// Software Creashes. i.e. importance in levels 1 2 3
+// Background Processes/Threads in Computers

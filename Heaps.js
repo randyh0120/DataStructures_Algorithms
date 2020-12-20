439e72a927ed029253d4ac749234546400e616eb
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-20 10:55:03 -0500

    Heaps: Definition

---
+++ Heaps.js
@@ -0,0 +1,41 @@
+// Heaps
+
+// What is a Binary Heap?
+// A Heap is similar to a BST, but has different rules
+// Like a BST a Binary Heap can only have 2 children per node.
+
+/*
+// Max Binary Heap:
+// Parent nodes are always larger than child nodes.
+// Each Parent has at most 2 children.
+// There is no order between siblings.
+// A binary heap is as compact as possible. All the children of each node are as full as they can be. The left children are filled out first.
+// They are not sorted like a BST.
+// All that matters is that the Parent Node is larger than the child.
+//          41
+//         /  \
+//       39    33
+//      /  \   /
+//     18  27 12
+*/
+
+/*
+// Min Binary Heap:
+// Parent Nodes are always smaller than child nodes
+// They are not sorted like a BST.
+// All that matters is that the Parent Node is smaller than the child
+//           0
+//         /   \
+//        2     3
+//      /  \   /  \
+//     17  19 36   7
+//    /  \
+//   25  100
+//
+*/
+
+/*
+// Use Cases
+// A Priority Queue is implemented using a Binary Heap
+// Graph Traversals
+*/

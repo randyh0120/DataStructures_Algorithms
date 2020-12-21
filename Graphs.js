595f6574e63af2cb20a8143d5952190228fb75c0
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-21 14:48:02 -0500

    Graph: Use Cases

---
+++ Graphs.js
@@ -0,0 +1,15 @@
+// Graphs
+
+// What is a Graph
+// A Graph is a collection of Nodes and the connection between those Nodes
+
+// Use Cases
+// Recommdation Engine like Netflix recommending a movie
+// Modeling Users in any social network
+// Facebook/Linkedin Friend Connection
+// Advertising Targeting
+// Location/Mapping
+// Routing Algorithms
+// Visual Hierarchy
+// File System Optimizations
+// They are using a Graph

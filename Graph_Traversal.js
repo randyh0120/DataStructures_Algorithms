236ea51b5fe6cef826dc147bf83489021285ccab
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-22 18:54:44 -0500

    GT: Use Cases

--- Graph_Traversal.js
+++ Graph_Traversal.js
@@ -1,1 +1,14 @@
 // Graph Traversal
+
+/*
+// Use Cases
+// Peer to Peer Networking
+// Web Crawlers
+// Finding "Closest" matches/recommendations
+// Shortest path problems
+//   GPS
+//   Solving Mazes
+//   AI (Shortest path to win the game)
+*/
+
+// We will visit every Node/Vertex in this example

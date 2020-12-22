0f7b14ae172ad05c4cf53a987bd29e35599b6521
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-21 21:44:19 -0500

    Graph: Updates

--- Graphs.js
+++ Graphs.js
@@ -4,6 +4,7 @@
 // A Graph is a collection of Nodes and the connection between those Nodes
 // The name Vertex and Node are interchangable
 
+/*
 // Use Cases
 // Recommdation Engine like Netflix recommending a movie
 // Modeling Users in any social network
@@ -14,9 +15,12 @@
 // Visual Hierarchy
 // File System Optimizations
 // They are using a Graph
+*/
 
+/*
 // Storing Graphs: Adjacency Matrix
 // We represent the data in a Matrix
 // All we store in the Matrix are the edges
 // Undirected Graphs are Symmetrical
 // Dirrected Graphs are NOT Symmetrical
+*/

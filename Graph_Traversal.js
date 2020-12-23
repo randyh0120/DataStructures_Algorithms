78b04a4cfb2e2b0b8b2e6fc6edcfe932b9b8abb9
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-22 20:39:21 -0500

    GT: Update

--- Graph_Traversal.js
+++ Graph_Traversal.js
@@ -24,4 +24,22 @@
 // 
 */
 
+// this will be a DFS Implementation
 // We will visit every Node/Vertex in this example
+
+/*
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B", "D")
+g.addEdge("C", "E")
+g.addEdge("D", "E")
+g.addEdge("D", "F")
+g.addEdge("E", "F")
+*/

0a1c33f18aa675923b7c3a2867f80bebf1809d08
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-24 17:38:25 -0500

    Dijkstras: Implementation od Add Vertex. Paused halfway through

--- Dijkastras_Algorithm.js
+++ Dijkastras_Algorithm.js
@@ -5,10 +5,22 @@
 // He advanced the field of computer science
 // Out of the 30 most influential papers he wrote some 6 of them.
 
-// Shortest Path
+// This algorithm is the Shortest Path
 
 // Use Cases
 // GPS - Finding Fastes Route
 // Network Routing - Finds open shortest path for data
 // Biology - Used to model the spread of viruses among humans
 // Airline Tickets - Finding cheapest route to your destination
+
+// We will implement a wieghted graph
+
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+}

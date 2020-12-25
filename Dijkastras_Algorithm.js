0602a8c5e42eb37981336c733fa0e961dea856e3
Rhattab0120 <randyhattab.one@gmail.com>
2020-12-25 13:11:34 -0500

    Dijkstras: Implementation

--- Dijkastras_Algorithm.js
+++ Dijkastras_Algorithm.js
@@ -1,19 +1,30 @@
 // Dijkstras Algorithm
 
+/*
 // Dijkstra was a Dutch Software Engineer, Physicist, and Essayist
 // He is a genius
 // He advanced the field of computer science
 // Out of the 30 most influential papers he wrote some 6 of them.
+*/
 
 // This algorithm is the Shortest Path
 
+/*
 // Use Cases
 // GPS - Finding Fastes Route
 // Network Routing - Finds open shortest path for data
 // Biology - Used to model the spread of viruses among humans
 // Airline Tickets - Finding cheapest route to your destination
+*/
 
+/*
+// Implementation
 // We will implement a wieghted graph
+//
+// Naive Priority Queue
+// We use a Priority Queue to know which node to go through next.
+// The smallest distance/weight will be added to the beginning.
+*/
 
 class WeightedGraph {
   constructor() {
